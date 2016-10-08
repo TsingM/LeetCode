cfd0306a0f3b6b0f4db2d6bbe74288604e3f559b
miaorq3 <miaorq3@yonyou.com>
2016-10-08 10:08:50 +0800

    345

--- code/345.js
+++ code/345.js
@@ -5,12 +5,15 @@ var reverseVowels = function(s) {
     var high = arr.length - 1;
     var vowels = ['a','A','e','E','o','O','i','I','u','U'];
     while(low < high) {
+        // 低位指针移动
         while((low < high) && (vowels.indexOf(arr[low]) == -1)) {
         	low++;
         }
+        // 高位指针移动
         while((low < high) && (vowels.indexOf(arr[high]) == -1)) {
         	high--;
         }
+        // 交换
         temp = arr[high];
     	arr[high] = arr[low];
     	arr[low] = temp;
