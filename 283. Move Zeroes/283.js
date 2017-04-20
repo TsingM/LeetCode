43b464444029eab45e128736da06e4d94104c8a9
TsingM <798026749@qq.com>
2017-04-20 20:26:27 +0800

    283

--- 283. Move Zeroes/283.js
+++ 283. Move Zeroes/283.js
@@ -3,9 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    var low = 0, high = nums.length - 1;
-    while(low < high) {
-        if(nums[low] == 0)
-        low++;
-    }
+    var count = 0;
+     for(var i = 0; i < nums.length; i++){
+            if(nums[i] === 0){
+                count++;
+            } else {
+                nums[i - count] = nums[i];
+                nums[i] = 0;
+            }
+        }
 }
