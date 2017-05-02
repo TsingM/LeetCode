0956410b1c94ef78e8c14d8ee3457c1523a0919c
TsingM <798026749@qq.com>
2017-05-02 10:59:23 +0800

    283完成

--- 283. Move Zeroes/283.js
+++ 283. Move Zeroes/283.js
@@ -3,13 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    var count = 0;
-     for(var i = 0; i < nums.length; i++){
-            if(nums[i] === 0){
-                count++;
-            } else {
-                nums[i - count] = nums[i];
-                nums[i] = 0;
-            }
+    var count = 0, length = nums.length;
+    for(var i = 0; i < length; i++){
+        if(nums[i] === 0) {
+            count++;
+        } else {
+            nums[i - count] = nums[i];
         }
+    }
+    for(i = length - count; i < length; i++) {
+        nums[i] = 0;
+    }
 }
