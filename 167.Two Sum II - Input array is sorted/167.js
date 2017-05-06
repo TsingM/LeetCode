61cbaec6935808fc1c0ccb9f31d97926ee1905f4
TsingM <798026749@qq.com>
2017-05-06 14:10:24 +0800

    167b

--- 167.Two Sum II - Input array is sorted/167.js
+++ 167.Two Sum II - Input array is sorted/167.js
@@ -4,6 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    var num = [];
-    
+    var low = 0, high = numbers.length - 1;
+    while(low < high) {
+        if(numbers[low] + numbers[high] == target) {
+            return [++low, ++high];
+        } else if(numbers[low] + numbers[high] > target) {
+            high--;
+        } else {
+            low++;
+        }
+    }
 };
